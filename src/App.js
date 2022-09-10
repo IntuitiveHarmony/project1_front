e4503581646a17a2f523431729788dd5ad1b8f4a
ununique3 <j.horst77@gmail.com>
2022-09-10 14:08:51 -0600

    front end started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import './App.css'
+import * as Tone from 'tone'
+import Step from './components/Step'
 
-function App() {
+const App = () => {
+  let [sequences, setSequences] = useState()
+  let [userSteps, setUserSteps] = useState(16)
+
+  //---------------------------------------------
+  //      steps grid based on user input
+  //---------------------------------------------
+  let steps = []
+  let division = 4
+
+  for (let i = 0; i < userSteps; i++) {
+    division -= 1
+    if (division >= 0) {
+      steps.push({position: i + 1, class:'step dark'})
+    } else {
+      steps.push({position: i + 1, class:'step light'})
+    }
+    if (division === -4) {
+      division = 4
+    }
+  }
+  const handleSteps = (event) => {
+    setUserSteps(event.target.value)
+  }
+
+  const getSequences = () => {
+    axios
+    .get('http://localhost:8000/api/sequencer')
+    .then(
+      (response) => setSequences(response.data),
+      (err) => console.error(err)
+    )
+   .catch((error) => console.error(error))
+ }
+
+  useEffect(() => {
+   getSequences()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h2>Hai</h2>
+      <h3>Sequence: </h3>
+      <form>
+        <select>
+          <option>PLaese</option>
+          <option>PLaese</option>
+          <option>PLaese</option>
+          <option>PLaese</option>
+          <option>PLaese</option>
+          <option>PLaese</option>
+        </select>
+      </form>
+      <input type='text' onChange={handleSteps} />
+        <div className='sequencerContainer'>
+          {steps.map((step) => {
+            return (
+              <>
+                <Step step={step} key={step.position}/>
+              </>
+            )
+          })}
+        </div>
+    </>
+  )
+
 }
 
 export default App;
